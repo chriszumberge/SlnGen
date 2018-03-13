6732377db595013b1b29f398e87e34a804d2cde9
chris <chris@DESKTOP-F4DMQ0I>
2018-03-12 20:46:33 -0400

    work in progress

---
+++ src/SlnGen.Core/ProjectFolder.cs
@@ -0,0 +1,34 @@
+﻿using SlnGen.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SlnGen.Core
+{
+    public class ProjectFolder : IFileContainer
+    {
+        readonly string _folderName;
+        public string FolderName => _folderName;
+
+        List<ProjectFile> _files = new List<ProjectFile>();
+        public List<ProjectFile> Files => _files;
+
+        List<ProjectFolder> _folders = new List<ProjectFolder>();
+        public List<ProjectFolder> Folders => _folders;
+
+        public ProjectFolder(string folderName)
+        {
+            _folderName = folderName;
+        }
+
+        List<ProjectFile> IFileContainer.GetFiles() => _files;
+
+        void IFileContainer.AddFile(ProjectFile file) => _files.Add(file);
+
+        List<ProjectFolder> IFileContainer.GetFolders() => _folders;
+
+        void IFileContainer.AddFolder(ProjectFolder folder) => _folders.Add(folder);
+    }
+}
---
+++ src/SlnGen.Core/Solution.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SlnGen.Core
+{
+    public class Solution
+    {
+        readonly string _solutionName;
+        public string SolutionName => _solutionName;
+
+        List<Project> _projects = new List<Project>();
+        public IReadOnlyList<Project> Projects => _projects.AsReadOnly();
+
+        readonly Guid _solutionGuid;
+        public Guid SolutionGuid => _solutionGuid;
+
+        public Solution(string solutionName) : this (solutionName, Guid.NewGuid()) { }
+
+        public Solution(string solutionName, Guid solutionGuid)
+        {
+            _solutionName = solutionName;
+            _solutionGuid = solutionGuid;
+        }
+
+        public Solution WithProject(Project project)
+        {
+            _projects.Add(project);
+            return this;
+        }
+    }
+}
