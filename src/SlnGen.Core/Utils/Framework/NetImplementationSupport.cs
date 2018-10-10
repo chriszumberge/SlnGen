c07394dd435fa0eca077561f7e06d4d402b767e0
chris <chris@DESKTOP-F4DMQ0I>
2018-10-10 14:01:52 -0400

    added Xamarin iOS platform support

--- src/SlnGen.Core/Utils/Framework/NetImplementationSupport.cs
+++ src/SlnGen.Core/Utils/Framework/NetImplementationSupport.cs
@@ -12,21 +12,21 @@ namespace SlnGen.Core.Utils
         static List<NetImplementation> s_implementations = new List<NetImplementation>
         {
             new NetImplementation{ NetStandard = NetStandardPlatform.v1_0, NetCore = NetCorePlatform.v1_0, NetFramework = NetFrameworkPlatform.v4_5,
-                                   XamarinAndroidPlatform = XamarinAndroidPlatform.v7_0},
+                                   XamarinAndroidPlatform = XamarinAndroidPlatform.v7_0, XamariniOSPlatform = XamariniOSPlatform.v10_0},
             new NetImplementation{ NetStandard = NetStandardPlatform.v1_1, NetCore = NetCorePlatform.v1_0, NetFramework = NetFrameworkPlatform.v4_5,
-                                   XamarinAndroidPlatform = XamarinAndroidPlatform.v7_0},
+                                   XamarinAndroidPlatform = XamarinAndroidPlatform.v7_0, XamariniOSPlatform = XamariniOSPlatform.v10_0},
             new NetImplementation{ NetStandard = NetStandardPlatform.v1_2, NetCore = NetCorePlatform.v1_0, NetFramework = NetFrameworkPlatform.v4_5_1,
-                                   XamarinAndroidPlatform = XamarinAndroidPlatform.v7_0},
+                                   XamarinAndroidPlatform = XamarinAndroidPlatform.v7_0, XamariniOSPlatform = XamariniOSPlatform.v10_0},
             new NetImplementation{ NetStandard = NetStandardPlatform.v1_3, NetCore = NetCorePlatform.v1_0, NetFramework = NetFrameworkPlatform.v4_6,
-                                   XamarinAndroidPlatform = XamarinAndroidPlatform.v7_0},
+                                   XamarinAndroidPlatform = XamarinAndroidPlatform.v7_0, XamariniOSPlatform = XamariniOSPlatform.v10_0},
             new NetImplementation{ NetStandard = NetStandardPlatform.v1_4, NetCore = NetCorePlatform.v1_0, NetFramework = NetFrameworkPlatform.v4_6_1,
-                                   XamarinAndroidPlatform = XamarinAndroidPlatform.v7_0},
+                                   XamarinAndroidPlatform = XamarinAndroidPlatform.v7_0, XamariniOSPlatform = XamariniOSPlatform.v10_0},
             new NetImplementation{ NetStandard = NetStandardPlatform.v1_5, NetCore = NetCorePlatform.v1_0, NetFramework = NetFrameworkPlatform.v4_6_1,
-                                   XamarinAndroidPlatform = XamarinAndroidPlatform.v7_0},
+                                   XamarinAndroidPlatform = XamarinAndroidPlatform.v7_0, XamariniOSPlatform = XamariniOSPlatform.v10_0},
             new NetImplementation{ NetStandard = NetStandardPlatform.v1_6, NetCore = NetCorePlatform.v1_0, NetFramework = NetFrameworkPlatform.v4_6_1,
-                                   XamarinAndroidPlatform = XamarinAndroidPlatform.v7_0},
+                                   XamarinAndroidPlatform = XamarinAndroidPlatform.v7_0, XamariniOSPlatform = XamariniOSPlatform.v10_0},
             new NetImplementation{ NetStandard = NetStandardPlatform.v2_0, NetCore = NetCorePlatform.v2_0, NetFramework = NetFrameworkPlatform.v4_6_1,
-                                   XamarinAndroidPlatform = XamarinAndroidPlatform.v8_0},
+                                   XamarinAndroidPlatform = XamarinAndroidPlatform.v8_0, XamariniOSPlatform = XamariniOSPlatform.v10_14},
         };
 
         public static NetImplementation GetMinCompatibleWith<T>(T targetNetPlatform) where T : NetPlatform
@@ -57,6 +57,10 @@ namespace SlnGen.Core.Utils
             {
                 return s_implementations.Where(x => x.XamarinAndroidPlatform == targetNetPlatform).ToList();
             }
+            else if (typeof(T) == typeof(XamariniOSPlatform))
+            {
+                return s_implementations.Where(x => x.XamariniOSPlatform == targetNetPlatform).ToList();
+            }
             else
             {
                 return new List<NetImplementation>();
@@ -70,5 +74,6 @@ namespace SlnGen.Core.Utils
         public NetCorePlatform NetCore { get; set; }
         public NetFrameworkPlatform NetFramework { get; set; }
         public XamarinAndroidPlatform XamarinAndroidPlatform { get; set; }
+        public XamariniOSPlatform XamariniOSPlatform { get; set; }
     }
 }
---
+++ src/SlnGen.Core/Utils/Framework/XamariniOSPlatform.cs
@@ -0,0 +1,16 @@
+﻿namespace SlnGen.Core.Utils
+{
+    public sealed class XamariniOSPlatform : NetPlatform
+    {
+        public static readonly XamariniOSPlatform v8_0 = new XamariniOSPlatform("Xamarin.iOS 8.0", "8.0.30703", "2.0");
+
+        public static readonly XamariniOSPlatform v10_0 = new XamariniOSPlatform("Xamarin.iOS 10.0", "", "");
+
+        public static readonly XamariniOSPlatform v10_14 = new XamariniOSPlatform("Xamarin.iOS 10.14", "", "");
+
+        public string SchemaVersion { get; }
+        private XamariniOSPlatform(string name, string productVersion, string schemaVersion) : base(name, productVersion) {
+            SchemaVersion = schemaVersion;
+        }
+    }
+}
